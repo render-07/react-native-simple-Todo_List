b9460059f18f72ed711a79b6028da448d0d56b41
render07 <59060987+render07@users.noreply.github.com>
2021-09-07 10:43:44 +0800

    iterate task using map

--- App.js
+++ App.js
@@ -6,16 +6,83 @@ export default function App() {
   const [task, setTask] = useState();
   const [taskItems, setTaskItems] = useState([]);
 
-  return (
-    
-  );
+  <View style={styles.container}>
+
+      {/* Today's Tasks */}
+      <View styles={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go */}
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key = {index} onPress = {() => completeTask(index)}>
+                  <Task jakol = {item}/>
+                </TouchableOpacity>
+              )
+            })
+
+
+          }
+        </View>
+      </View>
+      
+    </View>
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+    padding: 10
   },
+
+  taskWrapper:{
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+
+  sectionTitle:{
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 50
+  },
+  
+  items:{
+    marginTop: 30,
+  },
+
+  writeTaskWrapper:{
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+
+  input:{
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
+    width: 250,
+  },
+
+  addWrapper:{
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  addText:{
+
+  },
+
 });

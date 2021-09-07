623f273911dbd3a3533fdb3e633905b2fb1fbaa5
render07 <59060987+render07@users.noreply.github.com>
2021-09-07 10:41:13 +0800

    added styles

--- components/Task.js
+++ components/Task.js
@@ -12,3 +12,45 @@ export const Task = (props) => {
         </View>   
     )
 }
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: '#FFF',
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        marginBottom: 20,
+        justifyContent: 'space-between'
+    },
+
+    itemLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap'
+    },
+
+    square: {
+        width: 24,
+        height: 24,
+        backgroundColor: '#55BCF6',
+        opacity: 0.4,
+        borderRadius: 5,
+        marginRight: 15
+
+    },
+
+    itemText: {
+        maxWidth: '80%',
+    },
+
+    circular: {
+        width: 12,
+        height: 12,
+        borderColor: '#55BCF6',
+        borderWidth: 2,
+        borderRadius: 5
+    },
+
+});
